d9898d578b692e7d28f116fc455d4d0316de24ce
James <info@jamesattard.com>
2023-11-05 17:27:26 +0000

    Added three basic routes to demonstrate how routing works in Laravel

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,15 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/contacts', function() {
+    return "<h1>All Contacts</h1>";
+});
+
+Route::get('/contacts/create', function() {
+    return "<h1>Add new contacts</h1>";
+});
+
+Route::get('/contacts/{id}', function($id) {
+    return App\Models\Contact::find($id);
+});
