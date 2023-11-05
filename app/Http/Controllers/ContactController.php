1f91c93061050ee1665bb456be8606169f5e333c
James <info@jamesattard.com>
2023-11-05 18:11:41 +0000

    Created a ContactController and moved business logic from routes to this controller

---
+++ app/Http/Controllers/ContactController.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Contact;
+
+class ContactController extends Controller
+{
+    public function index()
+    {
+        return view('contacts.index');
+    }
+
+    public function create()
+    {
+        return view('contacts.create');
+    }
+
+    public function show()
+    {
+        $contact = Contact::find($id);
+        return view('contacts.show', compact('contact'));
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ContactController;
 
 /*
 |--------------------------------------------------------------------------
@@ -17,15 +18,8 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/contacts', function() {
-    return view('contacts.index');
-})->name('contacts.index');
+Route::get('/contacts', [ContactController::class, 'index'])->name('contacts.index');
 
-Route::get('/contacts/create', function() {
-    return view('contacts.create');
-})->name('contacts.create');
+Route::get('/contacts/create', [ContactController::class, 'create'])->name('contacts.create');
 
-Route::get('/contacts/{id}', function($id) {
-    $contact = App\Models\Contact::find($id);
-    return view('contacts.show', compact('contact'));
-})->name('contacts.show');
+Route::get('/contacts/{id}', [ContactController::class, 'show'])->name('contacts.show');
