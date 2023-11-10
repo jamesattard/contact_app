ba4f4c4ed8b9781921c575d54d3e8e9ee9c05533
James <info@jamesattard.com>
2023-11-10 09:25:24 +0000

    replaced content with the template table

--- resources/views/contacts/index.blade.php
+++ resources/views/contacts/index.blade.php
@@ -2,7 +2,118 @@
 
 
 @section('content')
-<h1>All contacts</h1>
-<a href="{{ route('contacts.create') }}">Add new</a>
-<a href="{{ route('contacts.show', 1) }}">Show contact</a>
+<main class="py-5">
+  <div class="container">
+    <div class="row">
+      <div class="col-md-12">
+        <div class="card">
+            <div class="card-header card-title">
+              <div class="d-flex align-items-center">
+                <h2 class="mb-0">All Contacts</h2>
+                <div class="ml-auto">
+                  <a href="form.html" class="btn btn-success"><i class="fa fa-plus-circle"></i> Add New</a>
+                </div>
+              </div>
+            </div>
+          <div class="card-body">
+            <div class="row">
+              <div class="col-md-6"></div>
+              <div class="col-md-6">
+                <div class="row">
+                  <div class="col">
+                    <div class="input-group mb-3">
+                      <select class="custom-select">
+                        <option value="" selected>All Companies</option>
+                        <option value="1">Company One</option>
+                        <option value="2">Company Two</option>
+                        <option value="3">Company Three</option>
+                      </select>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <table class="table table-striped table-hover">
+              <thead>
+                <tr>
+                  <th scope="col">#</th>
+                  <th scope="col">First Name</th>
+                  <th scope="col">Last Name</th>
+                  <th scope="col">Email</th>
+                  <th scope="col">Company</th>
+                  <th scope="col">Actions</th>
+                </tr>
+              </thead>
+              <tbody>
+                <tr>
+                  <th scope="row">1</th>
+                  <td>Alfred</td>
+                  <td>Kuhlman</td>
+                  <td>alfred@test.com</td>
+                  <td>Company one</td>
+                  <td width="150">
+                    <a href="show.html" class="btn btn-sm btn-circle btn-outline-info" title="Show"><i class="fa fa-eye"></i></a>
+                    <a href="form.html" class="btn btn-sm btn-circle btn-outline-secondary" title="Edit"><i class="fa fa-edit"></i></a>
+                    <a href="#" class="btn btn-sm btn-circle btn-outline-danger" title="Delete" onclick="confirm('Are you sure?')"><i class="fa fa-times"></i></a>
+                  </td>
+                </tr>
+                <tr>
+                  <th scope="row">2</th>
+                  <td>Frederick</td>
+                  <td>Jerde</td>
+                  <td>frederick@test.com</td>
+                  <td>Company one</td>
+                  <td>
+                    <a href="show.html" class="btn btn-sm btn-circle btn-outline-info" title="Show"><i class="fa fa-eye"></i></a>
+                    <a href="form.html" class="btn btn-sm btn-circle btn-outline-secondary" title="Edit"><i class="fa fa-edit"></i></a>
+                    <a href="#" class="btn btn-sm btn-circle btn-outline-danger" title="Delete" onclick="confirm('Are you sure?')"><i class="fa fa-times"></i></a>
+                  </td>
+                </tr>
+                <tr>
+                  <th scope="row">3</th>
+                  <td>Joannie</td>
+                  <td>McLaughlin</td>
+                  <td>joannie@test.com</td>
+                  <td>Company Two</td>
+                  <td>
+                    <a href="show.html" class="btn btn-sm btn-circle btn-outline-info" title="Show"><i class="fa fa-eye"></i></a>
+                    <a href="form.html" class="btn btn-sm btn-circle btn-outline-secondary" title="Edit"><i class="fa fa-edit"></i></a>
+                    <a href="#" class="btn btn-sm btn-circle btn-outline-danger" title="Delete" onclick="confirm('Are you sure?')"><i class="fa fa-times"></i></a>
+                  </td>
+                </tr>
+                <tr>
+                  <th scope="row">4</th>
+                  <td>Odie</td>
+                  <td>Koss</td>
+                  <td>odie@test.com</td>
+                  <td>Company Two</td>
+                  <td>
+                    <a href="show.html" class="btn btn-sm btn-circle btn-outline-info" title="Show"><i class="fa fa-eye"></i></a>
+                    <a href="form.html" class="btn btn-sm btn-circle btn-outline-secondary" title="Edit"><i class="fa fa-edit"></i></a>
+                    <a href="#" class="btn btn-sm btn-circle btn-outline-danger" title="Delete" onclick="confirm('Are you sure?')"><i class="fa fa-times"></i></a>
+                  </td>
+                </tr>
+                <tr>
+                  <th scope="row">5</th>
+                  <td>Edna</td>
+                  <td>Ondricka</td>
+                  <td>edna@test.com</td>
+                  <td>Company Three</td>
+                  <td>
+                    <a href="show.html" class="btn btn-sm btn-circle btn-outline-info" title="Show"><i class="fa fa-eye"></i></a>
+                    <a href="form.html" class="btn btn-sm btn-circle btn-outline-secondary" title="Edit"><i class="fa fa-edit"></i></a>
+                    <a href="#" class="btn btn-sm btn-circle btn-outline-danger" title="Delete" onclick="confirm('Are you sure?')"><i class="fa fa-times"></i></a>
+                  </td>
+                </tr>
+              </tbody>
+            </table> 
+
+
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</main>
 @endsection
