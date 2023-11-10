747d5a86883e7950984baadf0fa368b7780fbf29
James <info@jamesattard.com>
2023-11-10 09:36:17 +0000

    Added the filter partial view

---
+++ resources/views/contacts/_filter.blade.php
@@ -0,0 +1,18 @@
+<div class="row">
+  <div class="col-md-6"></div>
+  <div class="col-md-6">
+    <div class="row">
+      <div class="col">
+        <div class="input-group mb-3">
+          <select class="custom-select">
+            <option value="" selected>All Companies</option>
+            <option value="1">Company One</option>
+            <option value="2">Company Two</option>
+            <option value="3">Company Three</option>
+          </select>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
--- resources/views/contacts/index.blade.php
+++ resources/views/contacts/index.blade.php
@@ -16,24 +16,9 @@
               </div>
             </div>
           <div class="card-body">
-            <div class="row">
-              <div class="col-md-6"></div>
-              <div class="col-md-6">
-                <div class="row">
-                  <div class="col">
-                    <div class="input-group mb-3">
-                      <select class="custom-select">
-                        <option value="" selected>All Companies</option>
-                        <option value="1">Company One</option>
-                        <option value="2">Company Two</option>
-                        <option value="3">Company Three</option>
-                      </select>
-                      </div>
-                    </div>
-                  </div>
-                </div>
-              </div>
-            </div>
+
+            @include('contacts._filter')
+
             <table class="table table-striped table-hover">
               <thead>
                 <tr>
