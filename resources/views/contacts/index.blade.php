aec3a3ec55d6f527077803a7d89307440ff91039
James <info@jamesattard.com>
2023-11-10 09:26:12 +0000

    Replaced add new contact link with our named route

--- resources/views/contacts/index.blade.php
+++ resources/views/contacts/index.blade.php
@@ -11,7 +11,7 @@
               <div class="d-flex align-items-center">
                 <h2 class="mb-0">All Contacts</h2>
                 <div class="ml-auto">
-                  <a href="form.html" class="btn btn-success"><i class="fa fa-plus-circle"></i> Add New</a>
+                  <a href="{{ route('contacts.create') }}" class="btn btn-success"><i class="fa fa-plus-circle"></i> Add New</a>
                 </div>
               </div>
             </div>
