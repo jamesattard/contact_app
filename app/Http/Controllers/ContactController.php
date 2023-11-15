37f34911b2ef2819d5b5dadad5f06243e45c632b
James <info@jamesattard.com>
2023-11-15 14:19:53 +0000

    Amended the controller to return only results filtered by company_id request param

--- app/Http/Controllers/ContactController.php
+++ app/Http/Controllers/ContactController.php
@@ -11,18 +11,21 @@ class ContactController extends Controller
     public function index()
     {
         $companies = Company::orderBy('name')->pluck('name', 'id')->prepend('All Companies', '');
-        $contacts = Contact::all();
+        if (request('company_id') == null) {
+            $contacts = Contact::all();
+        } else {
+            $contacts = Contact::where('company_id', request('company_id'))->get();
+        }
+
         return view('contacts.index', compact('contacts', 'companies'));
     }
 
-    public function create()
-    {
+    function create() {
         return view('contacts.create');
     }
 
-    public function show($id)
-    {
+    function show($id) {
         $contact = Contact::find($id);
-        return view('contacts.show', compact('contact'));
+        return view('contacts.show', compact('contact')); // ['contact'] => $contact
     }
 }
--- public/js/app.js
+++ public/js/app.js
@@ -1,5 +1,4 @@
-document.getElementById('filter_company_id').addEventListener('change', 
-function(){
+document.getElementById('filter_company_id').addEventListener('change', function () {
   let companyId = this.value || this.options[this.selectedIndex].value
   window.location.href = window.location.href.split('?')[0] + '?company_id=' + companyId
 })
