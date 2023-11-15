79f2d3f897017ea4fb0f9663b480c2986e6ce612
James <info@jamesattard.com>
2023-11-15 08:07:29 +0000

    Added query string

--- public/js/app.js
+++ public/js/app.js
@@ -1,4 +1,5 @@
 document.getElementById('filter_company_id').addEventListener('change', 
 function(){
-  alert(this.value);
+  let companyId = this.value || this.options[this.selectedIndex].value
+  window.location.href = window.location.href.split('?')[0] + '?company_id=' + companyId
 })
