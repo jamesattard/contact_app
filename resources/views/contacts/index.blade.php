76a1c526a693a16ab9f52887d6fb031deab7a58f
James <info@jamesattard.com>
2023-11-15 18:23:51 +0000

    Amended the show button to redirect to the contacts show route

--- resources/views/contacts/index.blade.php
+++ resources/views/contacts/index.blade.php
@@ -1,6 +1,5 @@
 @extends('layouts.main')
 
-
 @section('content')
 <main class="py-5">
   <div class="container">
@@ -40,7 +39,7 @@
                       <td>{{ $contact->email }}</td>
                       <td>{{ $contact->company->name }}</td>
                       <td width="150">
-                        <a href="show.html" class="btn btn-sm btn-circle btn-outline-info" title="Show"><i class="fa fa-eye"></i></a>
+                        <a href="{{ route('contacts.show', $contact->id) }}" class="btn btn-sm btn-circle btn-outline-info" title="Show"><i class="fa fa-eye"></i></a>
                         <a href="form.html" class="btn btn-sm btn-circle btn-outline-secondary" title="Edit"><i class="fa fa-edit"></i></a>
                         <a href="#" class="btn btn-sm btn-circle btn-outline-danger" title="Delete" onclick="confirm('Are you sure?')"><i class="fa fa-times"></i></a>
                       </td>
